f20b7971e634230cc2a562cce8cee9cbad624330
CodeGallantX <johnayobami77@proton.me>
2024-07-10 06:43:53 +0100

    .

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
 // pages/index.js
+
 import React from 'react';
-import Head from 'next/head';
 import Navbar from '@/components/NavbarHome';
 import HeroSection from '@/components/HeroSection';
 import FeaturedList from '@/components/FeaturedList';
 import Footer from '@/components/Footer';
-// import Link from 'next/link';// 
 
 const HomePage = () => {
   return (
     <div>
-        <title>CHEF - Timbu Store</title>
+      <title>CHEF - Timbu Store</title>
       <Navbar />
       <HeroSection />
       <FeaturedList />
@@ -20,11 +19,3 @@ const HomePage = () => {
 };
 
 export default HomePage;
-
-
-
-/*I want to start a new next.js web app with react how do i do it?
-Its a store app with productlisting page and cart page.
-I won't be using typescript and tailwind.
-
-Give me code for the components and all the directory files*/
